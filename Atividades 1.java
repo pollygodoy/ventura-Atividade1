ffcc3917f7e46481f47c19e0b37a21caef774a12
Poliane F De Godoy <122944387+pollygodoy@users.noreply.github.com>
2023-03-08 18:38:22 -0300

    Add files via upload

---
+++ Atividades 1.java
@@ -0,0 +1,38 @@
+public class Ativade1 {
+    public static void main(String[] args) {
+        int matriza[] [] = {{1,2}, {4,5}};
+        int matrizb[] [] = {{7,8}, {10,11}};
+    
+        int matrizsoma [] [] = new int[2] [2];
+        int matrizmulti [] [] = new int[2] [2];
+
+        //soma 
+        for (int i = 0; i < matriza.length; i++) { //percorre linha da matriza
+        for (int j = 0; j < matriza.length; j++) {
+            matrizsoma [i] [j] = matriza[i] [j] + matrizb [i] [j];
+        
+        }
+      }
+      //multi
+      for (int i = 0; i < matriza.length; i++) { //percorre linha da matriza
+        for (int j = 0; j < matrizb.length; j++) {
+            matrizmulti [i] [j] = matriza[i] [j] + matrizb [i] [j];
+    }
+}
+
+//imprimir soma 
+for (int i = 0; i < matrizsoma.length; i++) { //percorre linha
+    System.out.println("\n");
+    for (int j = 0; j < matrizsoma.length; j++) {
+        matrizsoma [i] [j] = matrizsoma[i] [j] + ("\t");
+    }
+}
+
+//imprimir multiplicação
+for (int i = 0; i < matrizmulti.length; i++) { //percorre linha
+    System.out.println("\n");
+    for (int j = 0; j < matrizmulti.length; j++) {
+        matrizmulti [i] [j] = matrizmulti[i] [j] + ("\t");
+    }
+}
+
